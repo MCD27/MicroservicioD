b4a9264ea2339ec8d5fc1e147ef70387ac67719c
MCD27 <jsramosc@itc.edu.co>
2025-02-22 21:53:54 -0500

    Update database.py
    Se cambio la URL de la base de datos

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://jhon_dev:123456@localhost/universidadDB"
+DATABASE_URL = "postgresql://universidaddb_user:tBJNxMWU2oP9VwDAnLToB69gGCM1RCYX@dpg-cut7rbogph6c73b0hs8g-a/universidaddb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
